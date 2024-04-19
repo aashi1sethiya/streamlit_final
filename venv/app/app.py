51a7d18fb1c2d2bc116c26871a8cd1439dedf991
Aashi Sethiya <aashiyasethiya@gmail.com>
2024-04-19 14:42:42 -0400

    added streamlit

---
+++ venv/app/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Title
+st.title("Meal Tracker and Carbon Footprint Calculator")
+
+# Sidebar
+st.sidebar.title("Meal Selection")
+
+# Main Page Content
+st.write("Welcome to the Meal Tracker and Carbon Footprint Calculator App!")
