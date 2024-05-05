2a9782d3851cdc9b7ec5666ffb1ac82fabc896c8
Aashi Sethiya <aashiyasethiya@gmail.com>
2024-05-05 18:13:10 -0400

    added final changes

--- app.py
+++ app.py
@@ -1,4 +1,53 @@
 import streamlit as st
 from streamlit_echarts import st_echarts
 
-st.header('Find how much you are heating the globe and how you can cool it?')
+st.set_page_config(
+    page_title="Carbon Footprint Reduction App",
+    page_icon="♻🌏",
+)
+st.markdown(
+    """# 👣 **Carbon Footprint Reduction App**
+##### **Find how much you are contributing to heating the globe and how you can cool it?**
+
+
+## ♻ **Why CO2 Emissions Matter** 
+- CO2 emissions matter because they fuel **climate change**, **triggering extreme weather events** and **habitat loss**
+- Cutting them down is vital to safeguarding ecosystems and ensuring a **sustainable future**
+
+## 🌏 **Built for Empowering Environmental Consciousness**
+- Our Co2 Emissions Calculator is designed to shed light on your **carbon footprint** across three major domains: **Commute**, **Food**, and **Appliances**
+- With intuitive interfaces and powerful calculations, we aim to inspire informed decisions and meaningful actions towards a greener future
+
+## 🏗️ **How It Works**
+
+1. **Select Domain:** Choose a domain – Commute, Food, and Appliances –
+2. **Input Activities:** Specify your daily activities within the selected domain
+3. **Input Predictive Activities:** Choose activities that you could use instead of your usual activities to reduce Co2 Emissions
+4. **Calculate Emissions:** Compute your actual and predicted carbon footprint 
+5. **Explore Insights:** Understand visualizations of your actual and predicted Co2 emissions savings
+
+
+## 🛠️ **How It Is Built**
+Is is built with these core frameworks and modules:
+
+- **Streamlit** - To create the web app UI and interactivity
+- **SVG files** - To create visualisations that display beautiful graphs 
+- **E-Charts** - To create dynamic gauge charts
+- **Plotly** - To create interactive financial charts
+
+## 🎯 **Key Features**
+
+- **User-input data** - User's data for multiple daily activities
+- **Dynamic charts** - Interactive charts to understand impact of your predicted activities
+r
+"""
+)
+
+
+
+
+
+
+
+
+
